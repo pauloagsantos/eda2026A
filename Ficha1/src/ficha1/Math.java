058787a139a652e4d8e33e676ee9d4d4f07d941d
Paulo Santos <pauloagsantos@gmail.com>
2026-09-25 14:55:06 +0100

    analise da complexidade

---
+++ Ficha1/src/ficha1/Math.java
@@ -0,0 +1,25 @@
+package ficha1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author IPT
+ */
+public class Math {
+    
+    public static int factorialI(int num) {
+        int f=1;
+        for(int i = 1; i <= num; i++)
+            f *= i;
+        return f;
+    }
+    
+    public static int factorialR(int num) {
+        
+    }
+    
+}
